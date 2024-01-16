71c354b4bdd1704c21c7580eee1fca8f847f681a
ritik-prog <ritikmakhija2002@gmail.com>
2024-01-16 12:23:08 +0530

    Initial commit

---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,26 @@
+import { grey, deepOrange } from '@mui/material/colors';
+
+const theme = {
+    palette: {
+        black: {
+            main: grey[900],
+        },
+        orange: {
+            main: deepOrange[500],
+            light: deepOrange[100]
+
+        },
+        lightOrange: {
+            main: deepOrange[400]
+        },
+        superLightOrange: {
+            main: deepOrange[200]
+        },
+        lightGrey: {
+            main: grey[100]
+        }
+    },
+    fontFamily: 'Poppins'
+};
+
+export default theme;
